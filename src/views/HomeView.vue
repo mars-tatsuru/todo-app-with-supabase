aa37763d61131d15ee9a0a298e19a9165af4cee1
tatsuru koike <t-koike@marsflag.com>
2023-06-27 23:46:55 +0900

    feature: update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -307,7 +307,7 @@ onMounted(() => {
     margin-bottom: 30px;
 
     @media screen and (max-width: 767px) {
-      height: calc(100vh - 200px);
+      height: calc(100vh - 300px);
     }
 
     .todoListArea{
